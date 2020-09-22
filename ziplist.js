934c7aee738e59f32851ef4fb73d0857b4a28ec7
aklum3 <49923615+aklum3@users.noreply.github.com>
2020-09-21 22:54:09 -1000

    ziplist functions

---
+++ ziplist.js
@@ -0,0 +1,16 @@
+const test1 = [1, 2, 3];
+const test2 = ['a', 'b', 'c'];
+
+function zipList(list1, list2) {
+  let list = [];
+  for(let i = 0; i < list1.length; i++) {
+    list.push(list1[i], list2[i]);
+  }
+  return list;
+}
+console.log(zipList(test1, test2));
+
+function zipListTheSimpleWay(list1, list2) {
+  return _.flatten(_.zip(list1, list2));
+}
+console.log(zipListTheSimpleWay(test1, test2));
